ffd43598b2b8e322d057c13ff98c2f2b3c646910
amit.mish <amitm@media.net>
2020-03-15 12:31:58 +0530

    added server js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
