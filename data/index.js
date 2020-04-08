46ed8addbd7a96551bb159c102d6a5fc419d25a7
Paul Fourchon <fourchonpaul@gmail.com>
2020-04-08 22:52:49 +0200

    updated data

--- data/index.js
+++ data/index.js
@@ -1,38 +1,28 @@
 export const defaultTheme = 'dark'; // light
 
-export const Title = 'Amit Mishra ~ Senior Web Developer';
+export const Title = 'Paul Fourchon ~ Junior Web Developer';
 
 export const Resume = {
-    introduction: `{ 
-        Introduction = () => { 
-            Namaste: { 
-                Name: 'Amit Mishra', 
-                Designation: 'Senior Web Developer' 
-            } 
-        } 
+    introduction: `{
+        Introduction = () => {
+            Namaste: {
+                Name: 'Paul Fourchon',
+                Designation: 'Junior Web Developer'
+            }
+        }
 }`,
 
     experience: `{
         Experience = () => {
-            Media.net: {
-                company: 'Directi(Media.net)',
-                designation: 'Senior Web Developer',
-                period: 'Feb 2017 to Present'
+            Valority: {
+                company: 'Valority',
+                designation: 'Investment Advisor',
+                period: 'May 2018 to May 2019'
             },
-            Onlinemocks: {
-                company: 'Onlinemocks Pvt. Ltd.',
-                designation: 'UI Developer',
-                period: 'May 2016 to January 2017'
-            },
-            Collegedunia: {
-                company: 'Collegedunia Web Pvt. Ltd.',
-                designation: 'UI Developer',
-                period: 'October 2015 to April 2016'
-            },
-            Rising Hues: {
-                company: 'Rising Hues Technology LLP.',
-                designation: 'Web Developer',
-                period: 'July 2014 to September 2015'
+            Quintesens: {
+                company: 'Quintesens',
+                designation: 'Financial Services Consultant',
+                period: 'Jul 2016 to May 2018'
             }
         }
 }`,
@@ -43,89 +33,62 @@ export const Resume = {
                 html: 'Hyper Text Markup Language',
                 version: 5
             },
+            Ruby: {
+                ruby: 'Ruby',
+                framework: 'Rails'
+                version: 'ruby 2.6.5'
+            },
             Styling: {
                 css: 'Cascading Style Sheets',
                 sass: 'Syntactically Awesome Style Sheets',
-                postCss: 'PostCSS',
                 bootStrap: 'Bootstrap'
             },
             JavaScript: {
                 js: 'JavaScript',
-                react: 'React JS',
-                redux: 'Redux',
-                saga: 'Redux-Saga',
-                pwa: 'Progressive Web App',
                 node: 'Node JS'
             },
             BuildTools: {
                 webpack: 'Webpack',
-                gulp: 'Gulp'
             },
             Others: {
                 $: 'Jquery',
-                performance: 'Performance Optimization',
                 wd: 'Web Development',
                 rwd: 'Responsive Web Design',
-                validation: 'W3C Validation',
-                cbc: 'Cross-browser Compatibility',
                 vc: 'Version Control'
             },
             Tools: {
-                sketch: 'Sketch',
                 github: 'Github',
                 gitlab: 'Gitlab',
-                code: 'VS Code'
+                code: 'Sublime'
             }
         }
 }`,
 
-    projects: `{
-        Projects = () => {
-            zeta: {
-                url: <a target='_blank' href='https://www.zeta.tech/zeta-spotlight/'>'https://www.zeta.tech/zeta-spotlight'</a>
+    education: `{
+        "Education" = () => {
+            Rennes: {
+                school: 'Le Wagon',
+                degree: 'Web Applications Developer',
+                period: 'Feb 2020 to April 2020'
             },
-            Collegedunia: {
-                url: <a target='_blank' href='https://collegedunia.com/'>'https://collegedunia.com/'</a>
+            Brest: {
+                school: 'Brest Business School',
+                degree: 'Business Administration Bachelor',
+                period: 'Sep 2012 to May 2015'
             },
-            lib4dev: {
-                url: <a target='_blank' href='http://www.lib4dev.in/'>'http://www.lib4dev.in/'</a>,
-                github: <a target='_blank' href='https://github.com/amitmishrg/lib4dev'>'https://github.com/amitmishrg/lib4dev'</a>
-            },
-            Onlinemocks: {
-                url: <a target='_blank' href='http://onlinemocks.com/'>'http://onlinemocks.com/'</a>
-            }
-        }
-}`,
-
-    playground: `{
-        Playground = () => {   
-            Code Resume: {
-                url: <a target='_blank' href='https://code-resume.herokuapp.com/'>'https://code-resume.herokuapp.com/'</a>,
-                github: <a target='_blank' href='https://github.com/amitmishrg/code-resume'>'https://github.com/amitmishrg/code-resume'</a>
-            },    
-            offline News App: {
-                url: <a target='_blank' href='https://offline-news-app.herokuapp.com/'>'https://offline-news-app.herokuapp.com/'</a>,
-                github: <a target='_blank' href='https://github.com/amitmishrg/offline-news-app'>'https://github.com/amitmishrg/offline-news-app'</a>
-            },
-            CSS Animation: {
-                url: <a target='_blank' href='https://codepen.io/amitmishrg/pen/vxoWVd'>'https://codepen.io/amitmishrg/pen/vxoWVd'</a>,
-                github: <a target='_blank' href='https://github.com/amitmishrg/CSS-Designs-and-Animation'>'https://github.com/amitmishrg/CSS-Designs-and-Animation'</a>
-            },
-            Wrist Watch: {
-                url: <a target='_blank' href='https://codepen.io/amitmishrg/pen/evqeQp'>'https://codepen.io/amitmishrg/pen/evqeQp'</a>,
-                github: <a target='_blank' href='https://github.com/amitmishrg/wrist-watch'>'https://github.com/amitmishrg/wrist-watch'</a>
-            },
-            Mini Portfolio: {
-                url: <a target='_blank' href='https://codepen.io/amitmishrg/pen/pLELXZ'>'https://codepen.io/amitmishrg/pen/pLELXZ'</a>
+            Krakow: {
+                school: 'Uniwersytet Ekonomiczny w Krakowie',
+                degree: 'Business Administration Bachelor with Brest Business School',
+                period: 'Sep 2013 to May 2014'
             }
         }
 }`,
 
     contact: `{
-        Contact = () => {       
+        Contact = () => {
             {
-                email: 'amitmishra009v@gmail.com',
-                mobile: '8802100560'
+                email: 'fourchonpaul@gmail.com',
+                mobile: '06.87.91.48.22'
             }
         }
 }`
@@ -134,7 +97,7 @@ export const Resume = {
 
 export const SocialLinks = {
     resume: '/resume.pdf',
-    linkedin: 'https://www.linkedin.com/in/amitmishrg/',
-    twitter: 'https://twitter.com/amitmishrg',
-    github: 'https://github.com/amitmishrg'
+    linkedin: 'https://www.linkedin.com/in/paulfb/',
+    twitter: 'https://twitter.com/PaulF_B',
+    github: 'https://github.com/PFB28'
 }
